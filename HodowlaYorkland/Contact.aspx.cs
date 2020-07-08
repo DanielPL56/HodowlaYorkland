3272c0483f702e7f3f35f30f3fdc810edbb9ef55
Daniel Woelk <minijoker100@outlook.com>
2020-07-08 09:34:01 +0200

    removed useless method

--- HodowlaYorkland/Contact.aspx.cs
+++ HodowlaYorkland/Contact.aspx.cs
@@ -9,9 +9,5 @@ namespace HodowlaYorkland
 {
     public partial class Contact : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
